b53ad03072c4c720c1059dd2dcdc2af23db8b44c
DmooSoCool <DmooSoCool@gmail.com>
2018-03-09 17:16:28 +0800

    添加按钮示例

--- src/example/button.vue
+++ src/example/button.vue
@@ -1,15 +1,25 @@
 <template>
   <div id="example">
     <div class="demo-block-title">
-      <span>button按钮</span>
+      <span>按钮类型</span>
     </div>
 
     <loli-button type="default" tag="button" size="large" @click="log(1)">默认按钮</loli-button>
-    <loli-button type="default" tag="button" size="large" disabled @click="log(2)">禁用默认按钮</loli-button>
     <loli-button type="primary" tag="button" size="large">主要按钮</loli-button>
-    <loli-button type="primary" tag="button" size="large" disabled>禁用主要按钮</loli-button>
     <loli-button type="warning" tag="button" size="large">警告按钮</loli-button>
+
+    <div class="demo-block-title">
+      <span>禁用状态</span>
+    </div>
+    <loli-button type="default" tag="button" size="large" disabled @click="log(2)">禁用默认按钮</loli-button>
+    <loli-button type="primary" tag="button" size="large" disabled>禁用主要按钮</loli-button>
     <loli-button type="warning" tag="button" size="large" disabled>禁用警告按钮</loli-button>
+
+    <div class="demo-block-title">
+      <span>按钮大小</span>
+    </div>
+
+       
   </div>
 </template>
 <script>
